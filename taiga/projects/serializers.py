0b52fafffa7f7ba28af2adc45ddae5d02a53a0fc
David Barragán Merino <david.barragan@kaleidos.net>
2017-07-25 18:14:20 +0200

    Add game id

--- taiga/projects/serializers.py
+++ taiga/projects/serializers.py
@@ -526,6 +526,7 @@ class ProjectTemplateSerializer(serializers.LightSerializer):
 
 
 class GameSerializer(serializers.LightSerializer):
+    id = Field()
     uuid = Field()
     name = Field()
     project = Field(attr="project_id")
