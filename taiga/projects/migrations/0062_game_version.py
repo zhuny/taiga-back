97a1bb6bab86d48750b79d5cd0ac796edb529433
David Barragán Merino <david.barragan@kaleidos.net>
2018-09-11 10:39:32 +0200

    Update migrations

--- taiga/projects/migrations/0062_game_version.py
+++ taiga/projects/migrations/0062_game_version.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0060_auto_20170517_1359'),
+        ('projects', '0061_auto_20180911_1359'),
     ]
 
     operations = [
